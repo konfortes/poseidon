5f7a0bc2452c57b639e9fa4fa4158b77f1574f8d
konfortes <ronen.konfortes@riskified.com>
2020-11-04 13:31:21 +0200

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common'
 import { CommonModule } from './common/common.module'
 import { TelegramModule } from './telegram/telegram.module'
-import { Test } from './test';
 
 @Module({
   imports: [CommonModule, TelegramModule],
-  providers: [Test],
 })
 export class AppModule {}
