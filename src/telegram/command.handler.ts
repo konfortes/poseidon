9a6dc73b74f233c40d7a8c51b3eb2a4b94fa5046
konfortes <ronen.konfortes@riskified.com>
2020-11-04 18:12:27 +0200

    rates

--- src/telegram/command.handler.ts
+++ src/telegram/command.handler.ts
@@ -1,30 +1,82 @@
 import { Injectable } from '@nestjs/common'
-import { Help, Hears, Context, Command } from 'nestjs-telegraf'
+import {
+  Help,
+  Hears,
+  Context,
+  Command,
+  InjectBot,
+  TelegrafProvider,
+} from 'nestjs-telegraf'
 
 @Injectable()
 export class CommandHandler {
-  supportedCommands = []
+  constructor(@InjectBot() private bot: TelegrafProvider) {
+    this.bot.use(async (ctx, next) => {
+      ctx.foo = 123
+      await next()
+    })
+  }
+
+  SUBSCRIBE = 'subscribe'
+  UNSUBSCRIBE = 'unsubscribe'
+  FORECAST = 'forecast'
+  RATE = 'rate'
+
+  supportedCommands = [
+    this.SUBSCRIBE,
+    this.UNSUBSCRIBE,
+    this.FORECAST,
+    this.RATE,
+  ]
 
   @Help()
   help(ctx: Context) {
     ctx.reply("Please type '/' to see available commands")
   }
 
+  @Hears(/^[a-zA-Z].*/)
+  hears(ctx: Context) {
+    const supported = this.supportedCommands.map(c => '/' + c).join(',')
+    ctx.reply(`I only support ${supported}`)
+  }
+
   @Command('subscribe')
   subscribe(ctx: Context) {
     ctx.reply('you are now subscribed')
   }
+
   @Command('unsubscribe')
   unsubscribe(ctx: Context) {
     ctx.reply('you are now unsubscribed')
+    ctx.reply
   }
+
   @Command('forecast')
   forecast(ctx: Context) {
     ctx.reply("here is tomorrow's forecast")
   }
 
-  @Hears('hi')
-  hears(ctx: Context) {
-    ctx.reply('Hey there')
+  @Command('rate')
+  rate(ctx: Context) {
+    // TODO: memoize
+    const keyboardMarkup = {
+      reply_markup: this.rateKeyboard(),
+    }
+    ctx.reply('stars rate', keyboardMarkup)
+  }
+
+  rateKeyboard() {
+    return {
+      keyboard: [
+        [
+          { text: '1', request_location: true },
+          { text: '2', request_location: true },
+          { text: '3', request_location: true },
+          { text: '4', request_location: true },
+          { text: '5', request_location: true },
+        ],
+      ],
+      resizeKeyboard: true,
+    }
   }
 }
